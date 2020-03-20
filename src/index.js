00d70e4d122228370767c5cc4062a839465f03aa
amoncruz <amoncruz13@gmail.com>
2020-03-20 16:18:31 -0300

    configuração inicial do projeto usando webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+import axios from 'axios';
+import App from './app';
+
+
+ReactDOM.render(<App/>,document.getElementById("app"))
