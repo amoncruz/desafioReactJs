964acf0bd2820cb57ab2dc5b04a79cee27fca9fd
amoncruz <amoncruz13@gmail.com>
2020-03-21 12:16:03 -0300

    validação de formulario da tela de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,25 +2,56 @@ import React from 'react';
 import { Button, Form, FormGroup, Label, Input,Container } from 'reactstrap';
 import Menu from '../../components/NavBar';
 import Header from '../../components/Header';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 const Login = (props) => {
+
+    const SignInSchema = Yup.object().shape({
+        email: Yup.string()
+          .email('email inválido')
+          .required('Campo Obrigatório!'),
+        password: Yup.string()
+          .required('Campo Obrigatório!'),
+    });
+
   return (
     <>
             <Menu/>
             <Container fluid>
                 <Header title="Entre em sua Conta"/>
                     <div className="login-wrapper">
-                        <Form className="login-form">
+                    <Formik
+                    initialValues={{
+                        email: '',
+                        password: '',
+                    }}
+                    validationSchema={SignInSchema}
+                    onSubmit={values => {
+                        // same shape as initial values
+                        console.log(values);
+                    }}
+                    >
+                    {({ errors, touched,handleSubmit,handleChange }) => (
+                        <Form className="login-form" onSubmit={handleSubmit}>
                             <FormGroup>
                                 <Label for="exampleEmail">Email</Label>
-                                <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                                <Input type="email" name="email" id="exampleEmail" placeholder="email" onChange={handleChange} />
+                                {errors.email && touched.email ? (
+                                    <div className="error-message">{errors.email}</div>
+                                ) : null}
                             </FormGroup>
                             <FormGroup>
                                 <Label for="examplePassword">Password</Label>
-                                <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                                <Input type="password" name="password" id="examplePassword" placeholder="password" onChange={handleChange}/>
+                                {errors.password && touched.password ? (
+                                    <div className="error-message">{errors.password}</div>
+                                ) : null}
                             </FormGroup>
-                            <Button color="primary">Entrar</Button>
+                            <Button color="primary" type="submit">Entrar</Button>
                         </Form> 
+                          )}
+                    </Formik>
                     </div>
             </Container>
             </>
